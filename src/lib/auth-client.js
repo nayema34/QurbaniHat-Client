ab84089381b2d234fdf42f85f953d66c4b4f3987
nayema34 <jannatnayema2@gmail.com>
2026-06-30 22:52:58 +0600

    Updated auth-client.js

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,8 +1,8 @@
-import { createAuthClient } from "better-auth/react"
+import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3000"
-})
+  baseURL: process.env.BETTER_AUTH_URL || 
+           "http://localhost:3000"
+});
 
-// Fix: Destructure from the instance above instead of calling createAuthClient() again
 export const { signIn, signUp, useSession } = authClient;
